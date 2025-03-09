bba9da16054156814a761d195ba2c1ce3cf21c57
Saksham Arora <sakshamarora@Sakshams-MacBook-Air.local>
2025-03-09 22:58:17 +0530

    init

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// scroll the invitation video with the page only on big screens
+// var invitationVideo = document.getElementById("invitationVideo");
+
+// window.addEventListener("scroll", function () {
+//   var value = window.scrollY - 500;
+
+//   if (window.innerWidth > 768) {
+//     if (value < 0) {
+//       value = 0;
+//     } else if (value > 500) {
+//       value = 500;
+//     }
+
+//     invitationVideo.style.top = value * 0.5 + "px";
+//   }
+// });
